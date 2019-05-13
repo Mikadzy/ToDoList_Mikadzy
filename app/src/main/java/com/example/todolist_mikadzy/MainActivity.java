5bb0b503c42db16bfa46925f6af4582da91e0f13
Mikadzy <mikhailkemsu@gmail.com>
2019-05-14 01:36:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todolist_mikadzy/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.todolist_mikadzy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+
+//OnClickListener - отслеование нажатия
+//OnItemClickListener - отслежования нажатия в списке
+//Bundle - для времнного хранения даннных
+//Toats - вспылающие сообщения
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        itemET = findViewById(R.id.item_edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemsList = findViewById(R.id.items_list);
+
+        items = FileHelper.readData(this);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        itemsList.setAdapter(adapter);
+
+        btn.setOnClickListener(this);
+        itemsList.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+                FileHelper.writeData(items, this);
+                Toast.makeText(this, "Task added", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(items, this);
+        Toast.makeText(this, "Task removed", Toast.LENGTH_SHORT).show();
+    }
+}
